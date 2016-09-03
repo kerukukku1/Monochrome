8fff13364f585858705e802db64efb0f0473bc17
kerukukku1 <kerukukku1@gmail.com>
2016-09-03 12:20:44 +0900

    別コンストラクタを準備

--- src/Mahito6/Solver/Edge.java
+++ src/Mahito6/Solver/Edge.java
@@ -26,6 +26,23 @@ public class Edge {
 		this.distance = distance(kx1,ky1,kx2,ky2);
 	}
 	
+	public Edge(double kx1,double ky1,double kx2,double ky2){
+		this.kx1 = kx1;
+		this.ky1 = ky1;
+		this.kx2 = kx2;
+		this.ky2 = ky2;
+		if(kx1 > kx2){
+			///入れ替える
+			this.kx1 = kx2;
+			this.ky1 = ky2;
+			this.kx2 = kx1;
+			this.ky2 = ky1;
+		}
+		this.theta = calcTheta(kx1,ky1,kx2,ky2);
+		this.r = calcRadius(kx1,ky1,theta);
+		this.distance = distance(kx1,ky1,kx2,ky2);
+	}
+	
 	public Edge getExtensionEdge(double value){///線分の端点をvalueだけ伸ばしたエッジを返す
 		LinerMethod lm = new LinerMethod(new Point(kx1,ky1),new Point(kx2,ky2),1);
 		Point newL = lm.getPointFromLength(-value);
@@ -63,7 +80,14 @@ public class Edge {
 		return new Tuple2<Double,Double>(x,y);
 	}
 
-	public static double distance(double x,double y,double x1,double y1){///2点間の距離を返す
-		return Math.sqrt(Math.pow(x-x1, 2.0)+Math.pow(y-y1,2.0));
+	public static double distance(double x1,double y1,double x2,double y2){///2点間の距離を返す
+		return Math.sqrt(Math.pow(x1-x2, 2.0)+Math.pow(y1-y2,2.0));
+	}
+	
+	public double calcTheta(double x1,double y1,double x2,double y2){
+		return (Math.abs(y1-y2) < 0.000001)?Math.PI / 2 : Math.atan((x2 - x1)/(y1 - y2));
+	}
+	public double calcRadius(double x, double y, double theta){
+		return x*Math.cos(theta) + y*Math.sin(theta);	
 	}
 }
