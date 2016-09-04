ca85a76ec2f11123d3bc32e62d8bac23b39438e5
kerukukku1 <kerukukku1@gmail.com>
2016-09-04 17:28:52 +0900

    Problemの構成を変更

--- src/Mahito6/Main/Problem.java
+++ src/Mahito6/Main/Problem.java
@@ -1,6 +1,7 @@
 package Mahito6.Main;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,46 +17,32 @@ public class Problem {
 	public Mat source;
 	public Mat binImage, binImage2;
 	public BufferedImage bufBinImage;
-	public BufferedImage[] div;
+	public List<BufferedImage> div;
 	private List<Coordinates> coords;
 	public Problem(Mat source){
 		this.source = source;
-		div = new BufferedImage[100];
-		for(int i = 0; i < 100; i++){
-			div[i] = null;
-		}
+		div = new ArrayList<BufferedImage>();
 	}
 	
 	public void runAdaptiveThreshold(){
-        //枠のときの速度上げ専
-        //if(Constants.modeWaku)Imgproc.resize(src, src, new Size(), 0.50, 0.50, Imgproc.INTER_LINEAR);
-        //微妙?
-        //Imgproc.medianBlur(src, src, 1);
-        //if(!modeWaku)Imgproc.GaussianBlur(src, src, new Size(), 0.025);
         binImage = source.clone();
         binImage2 = source.clone();
-        //61 14 太いけど確実param　GAUSSIAN
-        //Imgproc.adaptiveThreshold(binaryAdaptive, binaryAdaptive, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 61, 14);
-        //Imgproc.adaptiveThreshold(binaryAdaptive, binaryAdaptive, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 71, 21);
-        //有力
         if(!Constants.modeWaku){
 //        	Imgproc.adaptiveThreshold(binImage, binImage, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 61, 14);
 //        	Imgproc.adaptiveThreshold(binImage, binImage, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 17, 8);
         	Imgproc.adaptiveThreshold(binImage, binImage, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 15, 8);
         	//nico nico
         	Imgproc.adaptiveThreshold(binImage2, binImage2, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 61, 14);	
-//            Imgproc.adaptiveThreshold(binImage, binImage, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 31, 8);
+//          Imgproc.adaptiveThreshold(binImage, binImage, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 31, 8);
         }
         Core.bitwise_and(binImage, binImage2, binImage);
-//        Highgui.imwrite("and_image.png", dst);
         //枠専用
         if(Constants.modeWaku){
         	Imgproc.adaptiveThreshold(binImage, binImage, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 21, 14);
         }
         bufBinImage = ImageManager.MatToBufferedImageBGR(binImage);
-        //Imgproc.resize(binaryAdaptive, binaryAdaptive, new Size(), 0.25, 0.25, Imgproc.INTER_LINEAR);
-        //Imgproc.resize(binaryAdaptive, binaryAdaptive, new Size(), 4.0, 4.0, Imgproc.INTER_LINEAR);
-        //黄色いプロット確認
+        
+//        黄色プロット確認
 //        if(Constants.isOutputDebugOval)confirm = MatToBufferedImageBGR(src);
 	}
 	
